0ca07707c390248b05fbd3b92dea241bd055b083
Bruno Gaspar <brunofgaspar1@gmail.com>
2014-09-15 22:50:28 +0100

    added the copyright headers
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- src/Container.php
+++ src/Container.php
@@ -1,4 +1,21 @@
 <?php namespace Cartalyst\Permissions;
+/**
+ * Part of the Permissions package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the Cartalyst PSL License.
+ *
+ * This source file is subject to the Cartalyst PSL License that is
+ * bundled with this package in the license.txt file.
+ *
+ * @package    Permissions
+ * @version    1.0.0
+ * @author     Cartalyst LLC
+ * @license    Cartalyst PSL
+ * @copyright  (c) 2011-2014, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
 
 use Closure;
 use Cartalyst\Support\Collection;
@@ -6,7 +23,7 @@ use Cartalyst\Support\Collection;
 class Container extends Collection {
 
 	/**
-	 * Returns a group instance
+	 * Returns a group instance.
 	 *
 	 * @param  mixed  $id
 	 * @param  \Closure  $callback
